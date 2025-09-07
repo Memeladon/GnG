88549799af01466b36de41bdd8d0f4b8be8b4a54
lardira <danilkefimov@gmail.com>
2025-09-07 18:26:48 +0300

    little refactoring

--- go_back/internal/services/user_service.go
+++ go_back/internal/services/user_service.go
@@ -7,13 +7,11 @@ import (
 	"gng/internal/utils/logger"
 )
 
-// UserService предоставляет бизнес-логику для пользователей
 type UserService struct {
 	db     *sql.DB
 	logger *logger.Logger
 }
 
-// NewUserService создает новый сервис пользователей
 func NewUserService(db *sql.DB, logger *logger.Logger) *UserService {
 	return &UserService{
 		db:     db,
@@ -21,27 +19,22 @@ func NewUserService(db *sql.DB, logger *logger.Logger) *UserService {
 	}
 }
 
-// GetByID получает пользователя по ID
 func (s *UserService) GetByID(ctx context.Context, id interface{}) (interface{}, error) {
 	return nil, nil
 }
 
-// Create создает нового пользователя
 func (s *UserService) Create(ctx context.Context, data interface{}) (interface{}, error) {
 	return nil, nil
 }
 
-// Update обновляет существующего пользователя
 func (s *UserService) Update(ctx context.Context, id interface{}, data interface{}) (interface{}, error) {
 	return nil, nil
 }
 
-// Delete удаляет пользователя по ID
 func (s *UserService) Delete(ctx context.Context, id interface{}) error {
 	return nil
 }
 
-// List получает список пользователей
 func (s *UserService) List(ctx context.Context, limit, offset int) ([]interface{}, error) {
 	return nil, nil
 }
